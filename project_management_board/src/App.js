f4b0864bc76d57afc20bde88c073d4c557e8dc1f
satchelsmidt <34499356+satchelsmidt@users.noreply.github.com>
2020-01-20 21:32:38 -0800

    navbar setup + various page links, dummy content added

---
+++ project_management_board/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
+import NavBar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route path='/' component={Dashboard}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ project_management_board/src/components/projects/ProjectList.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const ProjectList = () => {
+    return (
+        <div className="project-list section">
+            <div className="card z-depth-0 project-summaary">
+                <div className="card-content grey-text-text-darken-3">
+                    <span className="card-title">
+                        Project Title
+                    </span>
+                    <p>
+                        Posted by the Net Ninja
+                    </p>
+                    <p className="grey-text">
+                        3rd of Sept, 2 am
+                    </p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summaary">
+                <div className="card-content grey-text-text-darken-3">
+                    <span className="card-title">
+                        Project Title
+                    </span>
+                    <p>
+                        Posted by the Net Ninja
+                    </p>
+                    <p className="grey-text">
+                        3rd of Sept, 2 am
+                    </p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summaary">
+                <div className="card-content grey-text-text-darken-3">
+                    <span className="card-title">
+                        Project Title
+                    </span>
+                    <p>
+                        Posted by the Net Ninja
+                    </p>
+                    <p className="grey-text">
+                        3rd of Sept, 2 am
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectList
